e8e9a1bf58d19cafe003e6a4928fdc9f749fc9d3
Horacio G. de Oro <hgdeoro@gmail.com>
2011-10-21 21:45:04 -0300

    Agregue boton 'Convertir' y elimine codigo no usado.

--- convert.py
+++ convert.py
@@ -24,15 +24,11 @@ class TkFileDialog(Tkinter.Frame):
   def __init__(self, root):
 
     Tkinter.Frame.__init__(self, root)
-
     button_opt = {'fill': Tkconstants.BOTH, 'padx': 5, 'pady': 5}
-
-    # define buttons
     Tkinter.Button(self, text='Seleccion archivo de origen', command=self.askopenfile).pack(**button_opt)
-    #Tkinter.Button(self, text='askopenfilename', command=self.askopenfilename).pack(**button_opt)
-    #Tkinter.Button(self, text='asksaveasfile', command=self.asksaveasfile).pack(**button_opt)
     Tkinter.Button(self, text='Seleccion archivo destino', command=self.asksaveasfilename).pack(**button_opt)
-    #Tkinter.Button(self, text='askdirectory', command=self.askdirectory).pack(**button_opt)
+
+    Tkinter.Button(self, text="Convertir", command=self.convertir).pack(**button_opt)
 
     # define options for opening or saving a file
     self.file_opt = options = {}
@@ -43,36 +39,11 @@ class TkFileDialog(Tkinter.Frame):
     options['parent'] = root
     options['title'] = 'Seleccion de archivo...'
 
-    # This is only available on the Macintosh, and only when Navigation Services are installed.
-    #options['message'] = 'message'
-
-    # if you use the multiple file version of the module functions this option is set automatically.
-    #options['multiple'] = 1
-
   def askopenfile(self):
 
     """Returns an opened file in read mode."""
 
     return tkFileDialog.askopenfile(mode='r', **self.file_opt)
-
-    #  def askopenfilename(self):
-    #
-    #    """Returns an opened file in read mode.
-    #    This time the dialog just returns a filename and the file is opened by your own code.
-    #    """
-    #
-    #    # get filename
-    #    filename = tkFileDialog.askopenfilename(**self.file_opt)
-    #
-    #    # open file on your own
-    #    if filename:
-    #      return open(filename, 'r')
-
-    #  def asksaveasfile(self):
-    #
-    #    """Returns an opened file in write mode."""
-    #
-    #    return tkFileDialog.asksaveasfile(mode='w', **self.file_opt)
 
   def asksaveasfilename(self):
 
@@ -87,11 +58,8 @@ class TkFileDialog(Tkinter.Frame):
     if filename:
       return open(filename, 'w')
 
-    #  def askdirectory(self):
-    #
-    #    """Returns a selected directoryname."""
-    #
-    #    return tkFileDialog.askdirectory(**self.dir_opt)
+  def convertir(self):
+      pass
 
 if __name__=='__main__':
   root = Tkinter.Tk()
